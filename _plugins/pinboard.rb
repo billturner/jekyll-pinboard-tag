95854653ed84fa4cfe27f862e5b53ec809c549d2
Bill Turner <billturner@gmail.com>
2014-03-21 20:36:30 -0400

    Adding ability to specify tags and general cleanup
    * You can specify tags via pinboard tag or in config options
    * Limit the HTTP request to only fetch limit
    * Cleaned up how passed options are parsed/used
    * Probably some other things

--- _plugins/pinboard.rb
+++ _plugins/pinboard.rb
@@ -5,31 +5,39 @@
 #
 # Usage:
 #
-#   {% pinboard user:username limit:# %}
+#   {% pinboard user:username limit:# tags:tag,tag %}
 #
 # Example:
 #
-#   {% pinboard user:ericdfields limit:12 %}
+#   {% pinboard user:ericdfields limit:15 tags:ruby,rails %}
 #
 # All paramaters are optional, e.g.:
 #
 #   {% pinboard user:ericdfields %}
 #
-# Default Configuration (override in _config.yml):
+# The only parameters you can pass with the tag are: `user`, `limit`, and `tags`.
 #
-#   Pinboard_set:
-#     user:            'ericdfields'
-#     limit:  15
-#     list_tag:     'ol'
-#     list_class:   'pinboard_list'
-#     a_target:        '_blank'
+# Configuration options available for use in `_config.yml`:
 #
+#   pinboard_tag:
+#     user:       "billturner"
+#     limit:      15               # default
+#     list_tag:   "ol"             # default
+#     list_class: "pinboard_list"  # default
+#     a_target:   "_blank"         # default is ""
+#     tags:       "ruby,jekyll"
 #
+# UPDATES BY:
+# Bill Turner
+# http://github.com/billturner/jekyll-pinboard-tag
+# Plugin license: MIT
+#
+# ORIGINAL AUTHOR INFO:
 # Author: Eric D. Fields
 # Site: http://ericdfields.com
 # Twitter: @ericdfields
 # Email: ericdfields.com
-# Plugin Source: http://github.com/ericdfields/jekyll_Pinboard_set_tag
+# Plugin Source: http://github.com/ericdfields/Jekyll_Pinboard_Tag
 # Plugin License: MIT
 #
 
@@ -37,38 +45,26 @@ require 'net/http'
 require 'json'
 
 module Jekyll
-  class Pinboard < Liquid::Tag
-    def initialize(tag_name, config, token)
+  class PinboardTag < Liquid::Tag
+    def initialize(tag_name, options, token)
       super
 
-      @config = Jekyll.configuration({})['pinboard'] || {}
+      @options = tag_options(options)
+      @config = Jekyll.configuration({})['pinboard_tag'] || {}
 
-      userMatch = /(?<=user:)\w+/
-      limitMatch = /(?<=limit:)\w+/
+      @user = @options["user"] || @config["user"] || "billturner"
+      @limit = (@options["limit"] || @config["limit"] || 15).to_i
+      @tags = @options["tags"] || @config["tags"] || nil
 
-      if (config =~ userMatch) 
-        @user                    = userMatch.match(config)[0]
-      end
-      @user                      ||= @config['user']
-      @user                      ||= 'ericdfields'
-
-      if (config =~ limitMatch) 
-        @limit                   = limitMatch.match(config)[0].to_i
-      end
-      @limit                     ||= @config['limit']
-      @limit                     ||= 10 # max 50
-
-      @config['list_tag']        ||= 'ol'
-      @config['list_class']      ||= 'pinboard_list'
-      @config['is_list']         ||= true
-      @config['a_target']        ||= '_blank'
-      
+      @config["list_tag"] ||= "ol"
+      @config["list_class"] ||= "pinboard_list"
+      @config["a_target"] ||= ""
     end
 
     def render(context)
       <<-EOF
       <#{@config['list_tag']} class="#{@config['list_class']}">
-        #{bookmarks.collect {|bookmark| render_bookmark(bookmark)}.join}
+        #{bookmarks.collect{ |bookmark| render_bookmark(bookmark) }.join}
       </#{@config['list_tag']}>
       EOF
     end
@@ -76,61 +72,65 @@ module Jekyll
     def render_bookmark(bookmark)
       <<-EOF
       <li>
-        <a href="#{bookmark.url}" target="#{@config['a_target']}">
-          #{bookmark.description}
+        <a href="#{bookmark.url}" target="#{@config["a_target"]}">
+          #{bookmark.title}
         </a>
       </li>
       EOF
     end
 
     def bookmarks
-      @bookmarks = JSON.parse(json).take(@limit).map { |item| Bookmark.new(item['u'], item['d'], item['n'], item['dt'], item['a'], item['t'])
-      }
+      @bookmarks = JSON.parse(json).map do |item|
+        Bookmark.new(
+          url:          item["u"],
+          title:        item["d"],
+          description:  item["n"],
+          datetime:     item["dt"],
+          author:       item["a"],
+          tags:         item["t"]
+        )
+      end
     end
 
-    # Get feed with username
     def json
-      url     = 'http://feeds.pinboard.in/json/v1/u:' + @user
-      resp    = Net::HTTP.get_response(URI.parse(url))
-      return  resp.body
+      url = "http://feeds.pinboard.in/json/v1/u:#{@user}/"
+      unless @tags.nil?
+        url += @tags.split(",").map{ |t| "t:#{t}" }.join("/")
+        url += "/"
+      end
+      url += "?count=#{@limit}"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      return resp.body
+    end
+
+    private
+
+    def tag_options(options)
+      clean_options = {}
+      options = options.strip
+      options.split.each do |o|
+        next unless o =~ /:/
+        option = o.split(":")
+        next unless ["user", "limit", "tags"].include?(option[0])
+        next unless option[1]
+        clean_options[option[0]] = option[1]
+      end
+      clean_options
     end
   end
 
   class Bookmark
+    attr_reader :url, :title, :description, :datetime, :author, :tags
 
-    def initialize(url, description, n, datetime, author, tags)
-      @url              = url
-      @description      = description
-      @n                = n
-      @datetime         = datetime
-      @author           = author
-      @tags             = tags
-    end
-
-    def url
-      return @url
-    end
-
-    def description
-      return @description
-    end
-
-    def n
-      return @n
-    end
-
-    def datetime
-      return @datetime
-    end
-
-    def author
-      return @author
-    end
-
-    def tags
-      return @author
+    def initialize(params = {})
+      @url          = params[:url]
+      @title        = params[:title]
+      @description  = params[:description]
+      @datetime     = params[:datetime]
+      @author       = params[:author]
+      @tags         = params[:tags]
     end
   end
 end
 
-Liquid::Template.register_tag('pinboard', Jekyll::Pinboard)
+Liquid::Template.register_tag('pinboard', Jekyll::PinboardTag)
